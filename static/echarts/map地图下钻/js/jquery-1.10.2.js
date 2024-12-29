7efe521a1a2443427884d1cfd528df6c2f0e7578
wdtwo <wdtwo.com@gmail.com>
2024-12-29 10:13:44 +0800

    update

---
+++ static/echarts/map地图下钻/js/jquery-1.10.2.js
@@ -0,0 +1,3 @@
+/*-防止各大cdn公共库加载地址失效问题，此地址我们会时时监控，及时调整以保障正常访问-www.sucainiu.com为您服务*/
+/*当前为百度cdn公共库提供的jQuery-1.10.2*/
+document.write("<script src='https://s2.pstatp.com/cdn/expire-1-M/jquery/1.10.2/jquery.min.js'><\/script>");
